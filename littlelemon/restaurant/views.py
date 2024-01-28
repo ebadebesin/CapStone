cbb8fe4d305a4e7b9c3aff5473afff860c2b127c
Ebun Adebesin <adebesinebunoluwa1@gmail.com>
2024-01-28 01:06:19 -0800

    starting work

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
 # python manage.py migrate books
 # python manage.py makemigrations
 # for making migrations
+
+
+#def sayHello(request):
+ #return HttpResponse('Hello World')
+
+def index(request):
+   return render(request, 'index.html', {})
