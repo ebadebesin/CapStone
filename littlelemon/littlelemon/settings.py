07594072b91dc77a9c92da772e5d6ce81ff187f0
Ebun Adebesin <adebesinebunoluwa1@gmail.com>
2024-01-31 15:13:03 -0800

    djoser set up

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    # ... other settings ...
+}
+
+
+# Djoser 
+DJOSER={"USER_ID_FIELD":"username"}
+
